7ca725bf00d5462d4af01cf56d462badb8c258d6
ali <ali.naeim.gdrive@gmail.com>
2023-09-21 12:25:19 -0600

    add xgboost model

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import pickle
 import math
 from sklearn.metrics import mean_squared_error
 
@@ -29,7 +30,7 @@ def main():
         data_loader = dataloader.lstm.DataLoader(df, args)
         X_train, X_test, y_train, y_test = data_loader.load_data()
         model = load_model('./model_keeper/lstm_model.h5')
-        y_predict = model.predict(X_test)
+        y_pred = model.predict(X_test)
 
     if args.ml_model == 'prophet':
         data_loader = dataloader.neural_prophet.DataLoader(df, args)
@@ -37,7 +38,7 @@ def main():
         _save_model_dir_path = Path().cwd() / "model_keeper" / "prophet_model.json"
         with _save_model_dir_path.open('r') as fh:
             model = model_from_json(fh.read())
-        y_test,y_predict = model.predict(df)
+        y_test,y_pred = model.predict(df)
 
     if args.ml_model == 'neuralprophet':
         data_loader = dataloader.neural_prophet.DataLoader(df, args)
@@ -45,9 +46,18 @@ def main():
         _save_model_dir_path = Path().cwd() / "model_keeper" / "neural_prophet_model.json"
         with _save_model_dir_path.open('r') as fh:
             model = model_from_json(fh.read())
-        y_test,y_predict = model.predict(df)
+        y_test,y_pred = model.predict(df)
 
-    print(f"MSE: {math.sqrt(mean_squared_error(y_test,y_predict))}")
+    if args.ml == 'xgboost':
+        data_loader = data_loader.multi_variate.DataLoader(df, args)
+        X_train, X_test, y_train, y_test = data_loader.load_data()
+        _save_model_dir_path = models.xgboost.save_model_dir_path
+        with _save_model_dir_path.open('rb') as fh:
+            model = pickle.load(fh)
+        y_pred = model.predict(X_test).to_numpy().reshape(-1,)
+        y_test = y_test.to_numpy().reshape(-1,)
+
+    print(f"MSE: {math.sqrt(mean_squared_error(y_test,y_pred))}")
 
 
 if __name__ == "__main__":
