2fd323c206061409c0ffbe4bd289ecf79c2963d0
ali <ali.naeim.gdrive@gmail.com>
2023-09-21 02:54:16 -0600

    add argument and options

---
+++ config.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    add_arg = parser.add_argument
+    add_arg("--ml-model", default='lstm', choices=["lstm","prophet","neuralprophet"], type=str)
+    add_arg("--test-size", default=0.35, help="If float, should be between 0.0 and 1.0", type=int)
+    add_arg("--dataset", default="AAPL", choices=["AAPL","TSLA"])
+    add_arg("--time-stamp", default=100, type=int)
+    add_arg("--epoch", default=54)
+    add_arg("--batch-size", default=84)
+    add_arg("--enable-save-model", action="store_true")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    print(parse_args())
